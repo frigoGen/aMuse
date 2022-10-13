8d554abba47f42b94d1bcd3de52ff14dc3062961
Marta Michelle <mmcaliendo22@fed.idserve.net>
2022-10-13 12:17:01 +0200

    Initial Commit

---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Marta Michelle Caliendo on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
